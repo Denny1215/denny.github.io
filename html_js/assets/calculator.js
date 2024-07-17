99d526d80a7bf64504ff569f5a07ba0baaaadd33
accounts15 <accounts15@kodego.ph>
2024-07-17 18:49:15 +0800

    Introduction to JavaScript;

---
+++ html_js/assets/calculator.js
@@ -0,0 +1,36 @@
+document.getElementById("btn-add").addEventListener("click", function () {
+  let num1 = document.getElementById("num1").value; //string
+  let num2 = document.getElementById("num2").value;
+
+  let total = parseInt(num1) + parseInt(num2);
+  console.log(total);
+
+  document.getElementById("p-total").innerText = total;
+});
+
+document.getElementById("btn-sub").addEventListener("click", function () {
+  let num1 = document.getElementById("num1").value; //string
+  let num2 = document.getElementById("num2").value;
+
+  let total = parseInt(num1) - parseInt(num2);
+  console.log(total);
+  document.getElementById("p-total").innerText = total;
+});
+
+document.getElementById("btn-mul").addEventListener("click", function () {
+  let num1 = document.getElementById("num1").value; //string
+  let num2 = document.getElementById("num2").value;
+
+  let total = parseInt(num1) * parseInt(num2);
+  console.log(total);
+  document.getElementById("p-total").innerText = total;
+});
+
+document.getElementById("btn-div").addEventListener("click", function () {
+  let num1 = document.getElementById("num1").value; //string
+  let num2 = document.getElementById("num2").value;
+
+  let total = parseInt(num1) - parseInt(num2);
+  console.log(total);
+  document.getElementById("p-total").innerText = total;
+});
---
+++ html_js/assets/scripts.js
@@ -0,0 +1,58 @@
+//default fuction
+
+//Variables
+
+//string => alpha numeric ""
+//float => decimal point
+//integer => whole number
+//boolean = > true or false
+
+//null => null void
+//array => type that can store multiple data
+//object => type that can store multiple data with keys
+//undefined => unknown
+
+//var => pwede mong ihayag ulit, at pwede mong palitan yung laman
+//let => hindi mo pwedeng ihayag ulit, pero pwede mong palitan yung laman
+//const => hindi mo pwedeng ihayag ulit, hindi mo pwedeng palitan yung laman
+
+var name = "Denny"; //string
+var age = 34;
+
+console.log(name, age);
+
+//re-declare
+var age = age + 1;
+
+console.log(name, age);
+
+var age = age + 1;
+
+console.log(name, age);
+
+let job = "sales";
+console.log(name, age, job);
+job = "manager";
+console.log(name, age, job);
+const height = "5'7"; //constant
+console.log(name, age, job, height);
+
+//Math Operations
+
+let num1 = 45;
+let num2 = 20;
+
+//Addition
+console.log(num1 + num2);
+
+//Subtraction
+console.log(num1 - num2);
+
+//Multiplication
+console.log(num1 * num2);
+
+//Division
+console.log(num1 / num2);
+
+//Remainder
+console.log(num1 % num2);
