35d347cb39b298d9239a58d492441f17bce69448
accounts15 <accounts15@kodego.ph>
2024-08-09 18:40:38 +0800

    JS Array V2

---
+++ html_js_arrays_v2/assets/js/scripts.js
@@ -0,0 +1,20 @@
+let arrFruit = [
+  "item1", //0
+  "item2", //1
+  3, //2
+  1.45, //3
+  true, //4
+  ["internal item 1", "internal item 2"], //5
+  null, //6
+  undefined, //7
+];
+
+let arrName = ["Denny", "Gelilio", "Jesthony", "Morales"];
+
+let name = [...arrName, "M"]; //spread operator
+
+//destructuring
+let [denyFname, dennyLname, jessFname, jessLname] = arrName;
+
+let a = 5;
+let b = 10;
