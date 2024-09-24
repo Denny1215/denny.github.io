574bb623a35b5ecf15e0ff261e3904760e4f4a11
accounts15 <accounts15@kodego.ph>
2024-09-24 18:40:13 +0800

    Application Form v2;

--- application_form/select_data.php
+++ application_form/select_data.php
@@ -5,15 +5,59 @@
         die("Can't connect to the database!");
     }
 
-    $qSelect = "SELECT * FROM `db_denny_training`.`application_form`
-        WHERE 'id' = 4";
+    $qSelect = "SELECT * FROM `db_denny_training`.`application_form`";
         
         
     $eSelect = mysqli_query($db, $qSelect);
-
-    if ($eSelect == true) {
-        echo "Data selected!";
-    } else {
-        echo "Failed to select the data!";
+    if ($eSelect == false) {
+        die("Failed to fetch data!");
     }
+    
+    //returns number of rows fetched
+    $count = mysqli_num_rows($eSelect); //0
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link rel="stylesheet" href="assets/bootstrap-5/css/bootstrap.min.css" />
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-responsive table-hover">
+                    <thead>
+                        <th>ParentFirstName</th>
+                        <th>ParentLastName</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        
+                            if ($count > 0) {
+                                
+                                while($rows = mysqli_fetch_array($eSelect)) {
+                                    echo "<tr>
+                                        <td>".$rows['ParentFirstName']."</td>
+                                        <td>".$rows['ParentLastName']."</td>
+                                    </tr>";
+                                }
+                            }
+                        
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
